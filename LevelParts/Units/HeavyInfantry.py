0e8719db791bcf6e351d8ac5e4f471157ab78936
korliakov <Stratofortress_123#>
2020-12-06 16:59:06 +0300

    Изменил константы и добавил класс тяжелой пехоты

---
+++ LevelParts/Units/HeavyInfantry.py
@@ -0,0 +1,43 @@
+from Draws.UnitDraws import unit_draw
+from LevelParts.Units.UnitAI.UnitAI import *
+from LevelParts.Units.Unit import *
+
+
+class HeavyInfantry(Unit):
+
+    """
+
+    Class of first unit heavy infantry
+
+    : method __init__(): Initialise Heavy infantry. Do the super()__init__()
+                                   life = life of heavy unit
+                                   unit_type = "heavy_unit"
+                                   Receives side, coord
+    : method update(screen, level): Redraw city. Defining the action of the heavy unit
+    #: method reaction(level): Defining the action of the heavy unit
+    : method interaction_with_unit(self, unit: Unit, number, action): Method, that process situation, when self interact
+                                                                      with another unit. Heavy Infantry can just attack
+    : method interaction_with_district(self, unit: district, number, action): Method, that process situation, when self
+                                                                              interact  with another unit. Heavy
+                                                                              Infantry can just attack.
+    : method process_interaction(action):
+    """
+
+    def __init__(self, side, coord, image):
+        super().__init__(side, HeavyInfantryLife, coord, HeavyInfantryType, image, HeavyInfantryArmor,
+                         HeavyInfantryRange, HeavyInfantrySpeed, HeavyInfantryCooldown, HeavyInfantryDamage,
+                         HeavyInfantryDamageSpread, HeavyInfantrySideX, HeavyInfantrySideY)
+
+    def update(self, screen, level):
+        """
+
+        :param screen: Surface, where the picture is rendered
+        :param level: Level of the game
+
+        """
+        if self.interact_timer > 0:
+            self.interact_timer -= 1
+        unit_draw(self, self.side, self.position(level), screen, self.image, self.XSize, self.YSize)
+        info = Info(self, level)
+        solution = unitAI(info)
+        return self.reaction(solution, level)
